0f9fd277e322290e0301465ccb1d4e458978e628
Bastien <bastien@macbook-pro-de-bastien-1.home>
2020-06-27 09:54:57 +0200

    Iris EDA App deployment

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # iris-eda-app
 Iris EDA App with Streamlit
+
+# Required Files
+1 setup.sh
+2 Procfile
+3 requirements.txt
---
+++ iris_eda_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+def main():
+    """ Simple EDA App """
+
+    st.title("Iris EDA App with Streamlit")
+    st.subheader("Exploratory Data Analysis")
+
+
+if __name__ == "__main__":
+    main()
